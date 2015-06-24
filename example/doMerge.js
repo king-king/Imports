516322cd23e22951a976f04b25acd22bf2c36317
WQ <niuyiwangqun@sohu.com>
2015-06-24 14:38:20 +0800

    脚本合并

---
+++ example/doMerge.js
@@ -0,0 +1,7 @@
+/**
+ * Created by WQ on 2015/6/24.
+ */
+
+var merge = require( "../merge.js" );
+
+merge( "main.js", "../out/main.min.js" );
---
+++ merge.js
@@ -0,0 +1,163 @@
+/**
+ * Created by WQ on 2015/6/24.
+ */
+
+
+function resolve( base, path ) {
+    function clearFileName( path ) {
+        if ( path.indexOf( "." ) != -1 ) {
+            path = path.split( "/" ).slice( 0, -1 ).join( "/" );
+        }
+        return path;
+    }
+
+    base = clearFileName( base );
+    var block = path.split( "/" );
+    if ( block.length == 1 ) {
+        // path单纯是个文件名
+        return base + "/" + block[0];
+    }
+    else {
+        for ( var i = 0; i < block.length; i++ ) {
+            if ( block[i] == ".." ) {
+                // 回退
+                base = back( base );
+            }
+            else {
+                if ( base == "" ) {
+                    base = block[i];
+                }
+                else {
+                    base = base + "/" + block[i];
+                }
+            }
+        }
+        return base;
+    }
+}
+
+function back( path ) {
+    var block = path.split( "/" );
+    if ( block.length > 2 ) {
+        block = block.slice( 0, -1 );
+        return block.join( "/" );
+    }
+    else {
+        return "";
+    }
+}
+
+function map( array, func ) {
+    var mapArray = [];
+    array.forEach( function ( item, i ) {
+        mapArray.push( func( item, i ) );
+    } );
+    return mapArray;
+}
+
+// 顺序执行
+function serialTask( tasks, callback ) {
+    var index = 0;
+    tasks[index] && tasks[index]( function () {
+        ++index == tasks.length ? callback() : tasks[index]( arguments.callee );
+    } );
+}
+
+
+var fs = require( "fs" );
+
+function getFileName( path ) {
+    var blocks = path.split( "/" );
+    return blocks[blocks.length - 1];
+}
+
+function merge( path, outputPath ) {
+    var scripts = [];
+    var map = {};
+    var curPath = path;
+    // 添加min标记
+    //var writeFileName = getFileName( path ).split( "." );
+    //writeFileName = writeFileName[0] + ".min.js";
+    function readFile( path, callback ) {
+        fs.readFile( path, function ( err, data ) {
+            if ( err ) {
+                console.log( "err" + err );
+                callback( "" );
+            }
+            else {
+                callback( data );
+            }
+        } )
+    }
+
+    function getBetween( str, start, end ) {
+        var s = str.indexOf( start ) + 1;
+        str = str.replace( "\"", "%" );
+        return str.substring( s, str.indexOf( end ) );
+    }
+
+    function reverseArray( array, func ) {
+        // 反转数组
+        var a = [];
+        for ( var i = array.length - 1; i >= 0; i-- ) {
+            a.push( func( array[i] ) );
+        }
+        return a;
+    }
+
+    function getScript( str ) {
+        var ss = str.match( /imports((a-z|A-Z|"|.)*);/g );
+        if ( ss ) {
+            scripts = scripts.concat( reverseArray( ss, function ( item ) {
+                return resolve( curPath, getBetween( item, "\"", "\"" ) );
+            } ) );
+        }
+    }
+
+    function parse() {
+        function justDoIt() {
+            var s = scripts.pop();
+            curPath = s;
+            if ( s ) {
+                readFile( s, function ( data ) {
+                    getScript( data.toString() );
+                    map[s] = data.toString();
+                    justDoIt();
+                } );
+            }
+            else {
+                console.log( "end" );
+                console.log( JSON.stringify( map ) );
+                fs.writeFile( outputPath, JSON.stringify( map ), function ( err ) {
+                    if ( err ) {
+                        console.log( err );
+                    }
+                } );
+            }
+        }
+
+        justDoIt();
+    }
+
+
+    readFile( path, function ( data ) {
+        getScript( data.toString() );
+        parse();
+    } );
+
+}
+
+module.exports = merge;
+
+
+
+
+
+
+
+
+
+
+
+
+
