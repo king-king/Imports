9528ae8362f049de6a2e3094e33fafbc9cd4211f
qunwang <niuyiwangqun@sohu.com>
2015-06-27 22:30:49 +0800

    修改示例代码，添加忽略的文件

--- example/lib/layout/custom.js
+++ example/lib/layout/custom.js
@@ -2,18 +2,18 @@
  * Created by WQ on 2015/6/23.
  */
 Package( function ( exports ) {
-    var gn = imports( "getName.js" );
+	var gn = imports( "getName.js" );
 
-    function say() {
-        var name = gn.getName();
-        alert( name )
-    }
+	function say() {
+		var name = gn.getName();
+		console.log( name )
+	}
 
-    function see() {
-        console.log( "i see" )
-    }
+	function see() {
+		console.log( "i see" )
+	}
 
-    exports.say = say;
-    exports.see = see;
+	exports.say = say;
+	exports.see = see;
 
 } );
